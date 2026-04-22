ee16b920f81d01d8eaa86d64d56fb2b7ad0ef856
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-04-22 11:10:37 -0500

    replaced /products/{id}/remove-from-order with /lineitems/{id}

--- data/orders.js
+++ data/orders.js
@@ -16,13 +16,13 @@ export function getOrders() {
   })
 }
 
-export function completeCurrentOrder(orderId, payment_type) {
-  return fetchWithResponse(`orders/${orderId}`, {
+export function completeCurrentOrder(orderId, paymentTypeId) {
+  return fetchWithResponse(`orders/${orderId}/complete`, {
     method: 'PUT',
     headers: {
       Authorization: `Token ${localStorage.getItem('token')}`,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({payment_type})
+    body: JSON.stringify({paymentTypeId})
   })
 }
